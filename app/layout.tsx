5db37c9d9c7c6aa309a5ea07266c8f0bd2c18df9
Nabil Lambattan <lambattannabil2000@gmail.com>
2026-05-29 16:45:02 +0100

    WIP: save current state before i18n implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { ThemeProvider } from "@/components/theme-provider";
+// @ts-ignore: Importing global CSS as a side-effect.
 import "./globals.css";
 
 const _geist = Geist({ subsets: ["latin"] });
